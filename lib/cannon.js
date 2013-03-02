y-lohse/Cannon

commit 632bf119b2f9e2e79fb8e6b52554d0a5b18b3591

Prefixed private methods from LoadingSet

lib/cannon.js
@@ -487,7 +487,7 @@ Cannon.LoadingSet = Cannon.ClassFactory.extend({
 	add: function(piece, value){
 		value = (value || 1);
 		this.pieces.push({piece: piece, value: value, loaded: false});
-		this.update();
+		this.__update();
 
 		if (this.maxSize != 0 && this.pieces.length > this.maxSize){
 			Cannon.Logger.log('LoadingSet defined with a size of '+this.maxSize+' has been provided with '+this.pieces.length+' pieces', Cannon.Logger.Warning);
@@ -498,7 +498,7 @@ Cannon.LoadingSet = Cannon.ClassFactory.extend({
 	@param (mixed) piece The piece that just finished loading
 	*/
 	ready: function(piece){
-		var index = this.findPiece(piece);
+		var index = this.__findPiece(piece);
 		
 		if (index >= 0){
 			this.pieces[index].loaded = true;
@@ -509,7 +509,7 @@ Cannon.LoadingSet = Cannon.ClassFactory.extend({
 	Used to find the index of a piece inside the collection
 	@param (mixed) piece A piece that the LoadingSet watches
 	*/
-	findPiece: function(piece){
+	__findPiece: function(piece){
 		for (var i = 0; i < this.pieces.length; i++){
 			if (this.pieces[i].piece == piece) return i;
 		}
@@ -517,9 +517,9 @@ Cannon.LoadingSet = Cannon.ClassFactory.extend({
 		return false;
 	},
 	/**
-	Refreshes the amount of files loaded. You don'tneed to call this function yourself, it gets called when a piece has finished loading
+	Refreshes the amount of files loaded. You don't need to call this function yourself, it gets called when a piece has finished loading
 	*/
-	update: function(){
+	__update: function(){
 		this.total = 0;
 		this.loaded = 0;
 

commit 5524393ce56daa1af7ef0a0264c1adef7d1b7e50

Loadingset call update
Fixed call to update, broken after
632bf119b2f9e2e79fb8e6b52554d0a5b18b3591

lib/cannon.js
@@ -502,7 +502,7 @@ Cannon.LoadingSet = Cannon.ClassFactory.extend({
 		
 		if (index >= 0){
 			this.pieces[index].loaded = true;
-			this.update();
+			this.__update();
 		}
 	},
 	/**